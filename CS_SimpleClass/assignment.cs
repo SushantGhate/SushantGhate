17d118e0f1d46d1fa531adfc98d406647597d612
sushant.ghate <sushant.ghate@AZ-VDI-D1-234>
2022-05-20 01:49:51 -0700

    Add project files.

---
+++ CS_SimpleClass/assignment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CS_SimpleClass
+{
+    class assignment
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            operations op = new operations();
+            int result = op.calPower(2, 2);
+            Console.WriteLine($"Raise to values are: {result}");
+        }
+        
+    }
+}
---
+++ CS_SimpleClass/operations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_SimpleClass
+{
+    class operations
+    {
+        public int calPower(int a, int b)
+        {
+            if (b == 0)
+
+                return 1;
+            else
+                return a * calPower(a, b - 1);
+        }
+    }
+}
